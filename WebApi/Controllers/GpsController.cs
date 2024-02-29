36051bf8531d0bdad1d9c777749aacf1da6bd5c6
mucha-git <d.mucha@novatek.pl>
2024-02-29 18:36:47 +0100

    Gps Controller

---
+++ WebApi/Controllers/GpsController.cs
@@ -0,0 +1,16 @@
+﻿using WebApi.Models.Gps;
+
+namespace WebApi.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("[controller]")]
+public class GpsController : BaseController
+{
+    [HttpPost]
+    public ActionResult SetLocationData(GpsDataRequest request)
+    {
+        return Ok(request);
+    }
+}
---
+++ WebApi/Models/Gps/GpsDataRequest.cs
@@ -0,0 +1,10 @@
+namespace WebApi.Models.Gps;
+
+public class GpsDataRequest
+{
+    public int Id { get; set; }
+
+    public double Latitude { get; set; }
+
+    public double Longitude { get; set; }
+}
